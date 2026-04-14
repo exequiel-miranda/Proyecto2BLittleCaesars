f2f79f638ff15890105fafcd3c8e165bfefd0fd8
Exequiel <exequiel.miranda245@gmail.com>
2026-04-14 08:56:18 -0600

    commit

--- backend/config.js
+++ backend/config.js
@@ -7,4 +7,8 @@ export const config = {
   JWT: {
     secret: process.env.JWT_Secret_key,
   },
+  email:{
+    user_email: process.env.USER_EMAIL,
+    user_password: process.env.USER_PASSWORD
+  }
 };
--- backend/src/controllers/registerCustomerController.js
+++ backend/src/controllers/registerCustomerController.js
@@ -6,6 +6,7 @@ import bcryptjs from "bcryptjs"; //Encriptar contraseña
 import customerModel from "../models/customers.js";
 
 import { config } from "../../config.js";
+import { register } from "module";
 
 //array de funciones
 const registerCustomerController = {};
@@ -53,12 +54,94 @@ registerCustomerController.register = async (req, res) => {
       //#2- Secret key
       config.JWT.secret,
       //#3- ¿Cúando expira?
-      {expiresIn: "15m"}
+      { expiresIn: "15m" },
     );
 
     //guardamos el token en una cookie
-    res.cookie("registrationCookie", token, {maxAge: 15 * 60 * 1000})
+    res.cookie("registrationCookie", token, { maxAge: 15 * 60 * 1000 });
 
+    //ENVIAR CORREO ELECTÓNICO
+    //#1- Transporter -> ¿Quién lo envía?
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: config.email.user_email,
+        pass: config.email.user_password,
+      },
+    });
 
-  } catch (error) {}
+    //#2- mailOptions -> ¿Quíen lo recibe y cómo?
+    const mailOptions = {
+      from: config.email.user_email,
+      to: email,
+      subject: "Verificación de cuenta",
+      text:
+        "Para verificar tu cuenta, utiliza este código " +
+        randomCode +
+        " expira en 15 minutos",
+    };
+
+    //#3- Enviar el correo electrónico
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log("error");
+        return res.status(500).json({ message: "Error sending email" });
+      }
+
+      return res.status(200).json({ message: "Email sent" });
+    });
+  } catch (error) {
+    console.log("error" + error);
+    return res.status(500).json({ message: "Internal server eror" });
+  }
 };
+
+//VERIFICAR EL CÓDIGO QUE ACABAMOS DE ENVIAR
+registerCustomerController.verifyCode = async (req, res) => {
+  try {
+    //Solicitamos el código que el usuario escribió en el frontend
+    const { verificationCodeRequest } = req.body;
+
+    //Obtener el token de las cookies
+    const token = req.cookies.registrationCookie;
+
+    //Extraer todos los datos del token
+    const decoded = jsonwebtoken.verify(token, config.JWT.secret);
+    const {
+      randomCode: storedCode,
+      name,
+      lastName,
+      birthdate,
+      email,
+      password,
+      isVerified,
+      loginAttemps,
+      timeOut,
+    } = decoded;
+
+    if (verificationCodeRequest !== storedCode) {
+      return res.status(400).json({ message: "Invalid code" });
+    }
+
+    //Si todo está bien, y el usuario, lo registramos en la DB
+    const newCustomer = customerModel({
+      name,
+      lastName,
+      birthdate,
+      email,
+      password,
+      isVerified: true,
+    });
+
+    await newCustomer.save();
+
+    res.clearCookie("registrationCookie");
+
+    return res.status(200).json({ message: "Customer registered" });
+  } catch (error) {
+    console.log("error" + error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export default registerCustomerController;
